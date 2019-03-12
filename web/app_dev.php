8359f456563a7bae7d2bcf8af64d1f7a48d1a7c6
ROMEN <romenlee@gmail.com>
2019-03-12 23:27:04 +0200

    revert to scratch

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,8 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.//
-
+// Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '192.168.10.1'], true) || PHP_SAPI === 'cli-server')
